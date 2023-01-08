c5d9472dd10c6f02156aa55b27d57222c4f856a5
Tharwat <a.tharwat100@gmail.com>
2023-01-09 01:02:31 +0200

    Adding yaml files, Updating configurations

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a86767b0f2bdd4485ad5b5e0c52311a7-881220936.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
